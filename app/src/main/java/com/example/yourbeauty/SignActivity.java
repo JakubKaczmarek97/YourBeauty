edee3b7f6d0f218a606cce8351974f7ab5cd33d0
JakubKaczmarek97 <jakkac1997@gmail.com>
2019-10-28 11:11:19 +0100

    28.10.19 Dodanie Activity oraz Fragmentów Sign i Login. Stworzenie SignFragment (do poprawy widok horyzontalny).

--- app/src/main/java/com/example/yourbeauty/SignActivity.java
+++ app/src/main/java/com/example/yourbeauty/SignActivity.java
@@ -1,6 +1,31 @@
 package com.example.yourbeauty;
 
-public class SignActivity
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignActivity extends AppCompatActivity
 {
 
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager=(ViewPager) findViewById(R.id.fragment_container);
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new SignFragment(), "signFragment");
+        viewPager.setAdapter(adapter);
+    }
+    public void setViewPager(int FragmentNumber)
+    {
+        mViewPager.setCurrentItem(FragmentNumber);
+    }
 }
