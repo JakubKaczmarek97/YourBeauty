7234c589550da45d75184266c65af561bc6c3f3f
JakubKaczmarek97 <jakkac1997@gmail.com>
2019-10-28 14:08:49 +0100

    28.10.19 Rozpoczęcie prac nad MainMenuActivity oraz MainMenuFragment (kafelki)

---
+++ app/src/main/java/com/example/yourbeauty/MainMenuActivity.java
@@ -0,0 +1,29 @@
+package com.example.yourbeauty;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity
+{
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager=(ViewPager) findViewById(R.id.fragment_container);
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new MainMenuFragment(), "MainMenuFragment");
+        viewPager.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/yourbeauty/MainMenuFragment.java
+++ app/src/main/java/com/example/yourbeauty/MainMenuFragment.java
@@ -1,6 +1,5 @@
 package com.example.yourbeauty;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,29 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-public class LoginFragment extends Fragment
+public class MainMenuFragment extends Fragment
 {
 
-    private Button LogBtn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
 
-        View rootView = inflater.inflate(R.layout.login_fragment, container,
+        View rootView = inflater.inflate(R.layout.main_menu_fragment, container,
                 false);
-        LogBtn = (Button) rootView.findViewById(R.id.btnLogin);
-
-        LogBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),MainMenuActivity.class);
-                startActivity(intent);
-            }
-        });
 
 
         return rootView;
@@ -41,6 +28,5 @@ public class LoginFragment extends Fragment
         super.onViewCreated(view, savedInstanceState);
 
     }
-
 
 }
