d20188c803c23b10eaf479322e53d09ae7a3c553
JakubKaczmarek97 <jakkac1997@gmail.com>
2019-12-08 12:43:54 +0100

    Stworzenie prototypu logowania w SignInFragment.

--- app/src/main/java/com/example/yourbeauty/ui/SignIn/SignInFragment.java
+++ app/src/main/java/com/example/yourbeauty/ui/SignIn/SignInFragment.java
@@ -1,23 +1,132 @@
 package com.example.yourbeauty.ui.SignIn;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
 import androidx.fragment.app.Fragment;
 
-
 import com.example.yourbeauty.R;
+
+import java.util.Objects;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class SignInFragment extends Fragment
 {
+    private int SUCCESS ;
+
+    private ProgressDialog pDialog;
+
+    private EditText inputEmail;
+    private EditText inputPassword;
+
     public View onCreateView
             (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        inputEmail = view.findViewById(R.id.plainEmail);
+        inputPassword = view.findViewById(R.id.plainPass);
+
+        Button signIn = view.findViewById(R.id.btnSignIn);
+
+        signIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                new SignInFragment.UserLogin().execute();
+            }
+        });
+
+        return view;
+    }
+    class UserLogin extends AsyncTask<String, String, String>
+    {
+
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(getActivity());
+            pDialog.setMessage("Please wait to Sign In...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+
+            String Email = inputEmail.getText().toString();
+            String Password = inputPassword.getText().toString();
+
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody postData = new FormBody.Builder()
+                    .add("login", Email)
+                    .add("password", Password)
+                    .build();
+
+            String url_create_product = "http://10.0.2.2/bayb/login.php";
+            Request request = new Request.Builder()
+                    .url(url_create_product)
+                    .post(postData)
+                    .build();
+            try
+            {
+                Response response = client.newCall(request).execute();
+                String result = Objects.requireNonNull(response.body()).string();
+                System.out.println("Response:" + result);
+                SUCCESS = 1;
+                //return result;
+            }
+            catch (Exception e)
+            {
+                SUCCESS = 0;
+                System.out.println("Błąd: " + e);
+            }
+            return null;
+        }
+        protected void onPostExecute(String result)
+        {
+            pDialog.dismiss();
+            Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable()
+            {
+                public void run()
+                {
+                    if (SUCCESS == 1)
+                    {
+                        //Display success message
+                        Toast.makeText(getActivity(),
+                                "User logged successfully", Toast.LENGTH_LONG).show();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(),
+                                "Some error occurred while signing in",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
     }
 }
