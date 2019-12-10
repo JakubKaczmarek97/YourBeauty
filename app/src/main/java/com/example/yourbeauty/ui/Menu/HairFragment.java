99c19d95c4aa67da1434e792802b2aac0e2db282
JakubKaczmarek97 <jakkac1997@gmail.com>
2019-12-10 10:28:48 +0100

    Dynamicznie tworzone przyciski w HairFragment.

--- app/src/main/java/com/example/yourbeauty/ui/Menu/HairFragment.java
+++ app/src/main/java/com/example/yourbeauty/ui/Menu/HairFragment.java
@@ -1,21 +1,37 @@
 package com.example.yourbeauty.ui.Menu;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.yourbeauty.R;
 
-public class HairFragment extends Fragment
-{
+public class HairFragment extends Fragment {
     public View onCreateView
-            (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+            (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_hair, container, false);
+        View view = inflater.inflate(R.layout.fragment_hair, container, false);
+        LinearLayout linear = view.findViewById(R.id.fragment_hair);
+
+        for (int i = 1; i <= 20; i++) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            Button btn = new Button(getActivity());
+            btn.setId(i);
+            final int id_ = btn.getId();
+            btn.setText("button " + id_);
+            btn.setBackgroundColor(Color.rgb(70, 80, 90));
+            linear.addView(btn, params);
+
+        }
+        return view;
     }
 }
