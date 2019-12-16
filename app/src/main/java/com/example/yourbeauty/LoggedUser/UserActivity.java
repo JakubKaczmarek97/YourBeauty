f6dbd647fd251f85d06b55eb66f0dedeb85c461d
JakubKaczmarek97 <jakkac1997@gmail.com>
2019-12-16 13:19:57 +0100

    Rozpoczęcie prac nad UserActivity (zalogowany użytkownik).
    TODO: Stworzyć oddzielne pliki:
    app_bar_user.xml
    content_user.xml
    nav_header_user.xml
    activity_user_drawer.xml
    
    mobile_navigation.xml dla Usera
    
    W headerze dodać e-mail i nazwę zalogowanego użytkownika.

--- app/src/main/java/com/example/yourbeauty/LoggedUser/UserActivity.java
+++ app/src/main/java/com/example/yourbeauty/LoggedUser/UserActivity.java
@@ -3,12 +3,20 @@ package com.example.yourbeauty.LoggedUser;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.yourbeauty.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class UserActivity extends AppCompatActivity
 {
     private String userId;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,7 +26,27 @@ public class UserActivity extends AppCompatActivity
 
         userId = getIntent().getStringExtra("USER_ID");
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        System.out.println("UserActivity: " + userId);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder
+                (R.id.nav_home, R.id.nav_signin, R.id.nav_signup)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+    @Override
+    public boolean onSupportNavigateUp()
+    {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
